161123f5080d05de5df3c780f13701d20e22d82c
Martin Uhrin <martin.uhrin@gmail.com>
2018-01-27 14:57:23 +0100

    Added RMQ worker example
    Updated README

--- examples/rmq_rpc_client.py
+++ examples/rmq_rpc_client.py
@@ -3,7 +3,6 @@ from kiwipy.rmq import *
 communicator = RmqCommunicator(RmqConnector('amqp://localhost'))
 
 # Send an RPC message
-for _ in range(10000):
-    print(" [x] Requesting fib(30)")
-    response = communicator.rpc_send_and_wait('fib', 30)
-    print(" [.] Got %r" % response)
+print(" [x] Requesting fib(30)")
+response = communicator.rpc_send_and_wait('fib', 30)
+print(" [.] Got %r" % response)
