3af6cf1183c2594129614823b66d789ccab0e12b
cclauss <cclauss@bluewin.ch>
2018-09-24 10:45:58 +0100

    Undefined name: url --> 'url'
    __url__ is an _undefined name_ in this context which will probably raise __NameError__ at runtime.

--- examples/rmq_rpc_client.py
+++ examples/rmq_rpc_client.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from kiwipy import rmq
 
-communicator = rmq.RmqCommunicator.connect(connection_params={url: 'amqp://localhost'})
+communicator = rmq.RmqCommunicator.connect(connection_params={'url': 'amqp://localhost'})
 
 # Send an RPC message
 print(" [x] Requesting fib(30)")
