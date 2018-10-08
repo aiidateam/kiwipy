95d93f832020f4246e37eda814cabf1a85ef29e4
Martin Uhrin <martin.uhrin@epfl.ch>
2018-10-08 17:20:31 +0200

    Added python 3.4 support in setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
