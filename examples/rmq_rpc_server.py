7c04ff0f245b61386797452bf3d5588681a5c88e
Martin Uhrin <martin.uhrin@gmail.com>
2019-12-19 10:13:52 +0100

    Updated example scripts

--- examples/rmq_rpc_server.py
+++ examples/rmq_rpc_server.py
@@ -11,19 +11,22 @@ def fib(comm, num):
         return 1
     return fib(comm, num - 1) + fib(comm, num - 2)
 
-def fac(comm, n):
-    z=1
-    if n>1:
-        z=n*fac(comm, n-1)
-    return z
 
-try:
-    with rmq.RmqThreadCommunicator.connect(connection_params={'url': 'amqp://127.0.0.1'}) as communicator:
-        # Register an RPC subscriber with the name 'fib'
-        communicator.add_rpc_subscriber(fib, 'fib')
-        # Register an RPC subscriber with the name 'fac'
-        communicator.add_rpc_subscriber(fac, 'fac')
-        # Now wait indefinitely for fibonacci calls
-        threading.Event().wait()
-except KeyboardInterrupt:
-    pass
+def fac(comm, num):
+    result = 1
+    if num > 1:
+        result = num * fac(comm, num - 1)
+    return result
+
+
+if __name__ == "__main__":
+    try:
+        with rmq.RmqThreadCommunicator.connect(connection_params={'url': 'amqp://127.0.0.1'}) as communicator:
+            # Register an RPC subscriber with the name 'fib'
+            communicator.add_rpc_subscriber(fib, 'fib')
+            # Register an RPC subscriber with the name 'fac'
+            communicator.add_rpc_subscriber(fac, 'fac')
+            # Now wait indefinitely for fibonacci calls
+            threading.Event().wait()
+    except KeyboardInterrupt:
+        pass
